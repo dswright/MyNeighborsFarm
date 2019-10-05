e23cf38b75555f8515e5543ccb822d58192f07c9
Dylan Wright <dylansamuelwright@gmail.com>
2019-10-04 18:24:10 -0700

    move to yarn

--- src/components/UsageHero.js
+++ src/components/UsageHero.js
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export default ({ page }) => (
   <div>
-    <img
-      src='https://cdn.reactlandia.com/faceyspacey-white-logo.png'
-      alt='FaceySpacey'
-    />
-
     <div>
       <h2>{'universal(props => import(`./${props.page}`))'}</h2>
       <h3>
@@ -16,4 +11,4 @@ export default ({ page }) => (
       </h3>
     </div>
   </div>
-)
+);
