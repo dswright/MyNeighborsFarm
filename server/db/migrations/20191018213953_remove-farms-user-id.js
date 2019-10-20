153a180f9ef0ac6b2e6979f774602f92db88f79b
Dylan Wright <dylansamuelwright@gmail.com>
2019-10-20 11:09:11 -0700

    fix migrations

--- server/db/migrations/20191018213953_remove-farms-user-id.js
+++
@@ -1,7 +0,0 @@
-exports.up = (knex) => knex.schema.table('farms', (table) => {
-  table.dropColumn('userId');
-});
-
-exports.down = (knex) => knex.schema.table('farms', (table) => {
-  table.boolean('userId');
-});
