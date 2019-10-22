76ce8ce893cb7f20a0ef550a37cfd815dda0d6df
Dylan Wright <dylansamuelwright@gmail.com>
2019-10-21 22:28:57 -0700

    add back favicon

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ const csrfProtection = csrf({ cookie: true });
 
 const app = express();
 
+// ignore favicon requests
+app.use((req, res, next) => {
+  if (req.originalUrl && req.originalUrl.split('/').pop() === 'favicon.ico') {
+    return res.sendStatus(204);
+  }
+  return next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/', csrfProtection, authenticateMiddleware);
