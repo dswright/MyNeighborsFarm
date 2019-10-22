1bb97690a696e06b0ffbbe7e43a7f7db1e0a3908
Dylan Wright <dylansamuelwright@gmail.com>
2019-10-21 17:26:48 -0700

    remove unnecessary migration

--- server/db/migrations/20191020192645_add-product-variation-id-col.js
+++
@@ -1,7 +0,0 @@
-exports.up = (knex) => knex.schema.table('farm_products', (table) => {
-  table.integer('productVariationId').unsigned();
-});
-
-exports.down = (knex) => knex.schema.table('farm_products', (table) => {
-  table.dropColumn('productVariationId');
-});
